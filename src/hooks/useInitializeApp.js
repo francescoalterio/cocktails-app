fbb8954850e838a5c146e41da19a1b757b2512a1
Francesco Alterio <francescoalteriog@gmail.com>
2022-07-21 12:58:32 -0400

    add favorite system (add and remove favorite drinks)

--- src/hooks/useInitializeApp.js
+++ src/hooks/useInitializeApp.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setCocktailsList } from "../store/slices/cocktailsListSlice";
 import { setLongList } from "../store/slices/longListSlice";
 import { setNonList } from "../store/slices/nonListSlice";
+import { selectFavorites } from "../utils/selectFavorites";
 
 const cocktailsURL =
   "https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass";
@@ -16,17 +17,40 @@ const nonURL =
 const useInitializeApp = () => {
   const dispatch = useDispatch();
 
-  fetch(cocktailsURL)
-    .then((res) => res.json())
-    .then((result) => dispatch(setCocktailsList(result.drinks)));
+  useEffect(() => {
+    fetch(cocktailsURL)
+      .then((res) => res.json())
+      .then((result) => {
+        selectFavorites(result.drinks).then((result) => {
+          const favoritesDesordered = result.sort(function (a, b) {
+            return Math.random() - 0.5;
+          });
+          dispatch(setCocktailsList([...favoritesDesordered]));
+        });
+      });
 
-  fetch(longURL)
-    .then((res) => res.json())
-    .then((result) => dispatch(setLongList(result.drinks)));
+    fetch(longURL)
+      .then((res) => res.json())
+      .then((result) => {
+        selectFavorites(result.drinks).then((result) => {
+          const favoritesDesordered = result.sort(function (a, b) {
+            return Math.random() - 0.5;
+          });
+          dispatch(setLongList([...favoritesDesordered]));
+        });
+      });
 
-  fetch(nonURL)
-    .then((res) => res.json())
-    .then((result) => dispatch(setNonList(result.drinks)));
+    fetch(nonURL)
+      .then((res) => res.json())
+      .then((result) => {
+        selectFavorites(result.drinks).then((result) => {
+          const favoritesDesordered = result.sort(function (a, b) {
+            return Math.random() - 0.5;
+          });
+          dispatch(setNonList([...favoritesDesordered]));
+        });
+      });
+  }, []);
 
   return;
 };
