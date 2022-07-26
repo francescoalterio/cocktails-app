eb4e6db7c292ba5577630c4cd2405263e91ee854
Francesco Alterio <francescoalteriog@gmail.com>
2022-07-26 16:08:53 -0400

    move app into src

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import store from "./src/store";
+import store from "./store";
 import { Provider } from "react-redux";
 import { useFonts } from "expo-font";
-import BackgroundGradient from "./src/components/BackgroundGradient";
-import TabBar from "./src/screens/TabBar";
-import Drink from "./src/screens/Drink";
+import BackgroundGradient from "./components/BackgroundGradient";
+import TabBar from "./screens/TabBar";
+import Drink from "./screens/Drink";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [loaded] = useFonts({
-    Nanum: require("./assets/fonts/NanumMyeongjo-Regular.ttf"),
+    Nanum: require("../assets/fonts/NanumMyeongjo-Regular.ttf"),
   });
 
   return loaded ? (
